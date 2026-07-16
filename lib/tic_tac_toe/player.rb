63bb8714ee515007dd81b5f082517738fa06415a
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-16 08:41:42 +0200

    Create Player class with name and color attributes

--- lib/tic_tac_toe/player.rb
+++ lib/tic_tac_toe/player.rb
@@ -1,1 +1,16 @@
-puts 'Hey, this is player class!'
+# Player class: responsible for player creation
+# Attributes: player's name and color of choice
+# Notes:
+# - symbol or mark is missing like X or O
+# - name set should not be availible
+# - if name is not set, id of player should increment like...
+# ...player1 and player2 or variable name
+class Player
+  attr_reader :name
+  attr_accessor :color
+
+  def initialize(color, name = 'player1')
+    @name =  name
+    @color = color
+  end
+end
