78ae977dae02deed5bf4603d0c3356e2977edf77
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-18 00:17:42 +0200

    Validate marker to be X or O

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # gem "rails"
 
-gem "rubocop", "~> 1.88"
+gem 'rubocop', '~> 1.88'
--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -12,10 +12,24 @@ class Game
   def set_players
     puts 'Welcome'
     puts 'Player1, please enter your marker:'
-    @player1 = Player.new(gets)
+    @player1 = Player.new(validate_player_marker)
     puts "Player1: #{@player1.marker}"
     puts 'Player2, please enter your marker:'
-    @player2 = Player.new(gets)
+    @player2 = Player.new(validate_player_marker)
     puts "Player2: #{@player2.marker}"
+  end
+
+  def validate_player_marker
+    pass = true
+    while pass
+      input = gets.chomp
+      if %w[X O].include?(input) && input.length == 1
+        pass = false
+        return input
+      else
+        pass = true
+        puts 'Enter X or O'
+      end
+    end
   end
 end
