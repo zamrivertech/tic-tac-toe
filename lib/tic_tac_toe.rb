b50a5c470ac6b599acb08acceeeca640e2c3d325
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-17 23:25:04 +0200

    Set and get player in Game class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,4 @@
 require_relative 'tic_tac_toe/player'
 require_relative 'tic_tac_toe/game'
 
-puts 'Welcome'
-
-puts 'Player1, please enter your marker:'
-
-player1 = Player.new(gets)
-
-puts "Player1: #{player1.marker}"
-
-puts 'Player2, please enter your marker:'
-
-player2 = Player.new(gets)
-
-puts "Player2: #{player2.marker}"
+Game.new
--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,4 +1,21 @@
 # Game class: responsible for game logic and flow
 # Attributes:
 class Game
+  attr_reader :player1, :player2
+
+  def initialize
+    @player1 = Player
+    @player2 = Player
+    set_players
+  end
+
+  def set_players
+    puts 'Welcome'
+    puts 'Player1, please enter your marker:'
+    @player1 = Player.new(gets)
+    puts "Player1: #{@player1.marker}"
+    puts 'Player2, please enter your marker:'
+    @player2 = Player.new(gets)
+    puts "Player2: #{@player2.marker}"
+  end
 end
