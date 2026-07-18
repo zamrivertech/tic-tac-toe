6b64b8089f622731edb294a7a031b3b2506d275d
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-18 19:28:53 +0200

    Display board

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,9 +1,18 @@
 class Board
   def initialize
-    @board_array = [[], [], [], [], [], [], [], [], []]
+    @board_array = [[0], [1], [2], [3], [4], [5], [6], [7], [9]]
   end
 
   def display_grid
-    p @board_array
+    count = 0
+    @board_array.each do |item|
+      count += 1
+      if [3, 6, 9].include?(count)
+        print item
+        puts "\n"
+      else
+        print item
+      end
+    end
   end
 end
